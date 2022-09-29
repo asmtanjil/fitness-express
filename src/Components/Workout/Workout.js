c1c9a90743c1610658d559aea25f4d9a14e6de83
asmtanjil <asm.tanjil@gmail.com>
2022-09-29 12:59:45 +0600

    Data Loaded TO Cart Dynamically

--- src/Components/Workout/Workout.js
+++ src/Components/Workout/Workout.js
@@ -5,16 +5,7 @@ const Workout = ({exercise}) => {
   // const {exercise} = exercise;
   const {name, image, age, time, about} = exercise;
   return (
-    <div className='workout-container'>
-      <img src={image} alt="" />
-    <div className='workout-info'>
-      <h4>{name}</h4>
-      <p className='workout-data'>{about}</p>
-      <p>For Age: <strong>{age}+</strong></p>
-      <p>Time Required: <strong>{time}s</strong></p>
-    </div>
-    <button className='btn-add'><p>Add To List</p></button>
-    </div>
+    
   );
 };
 
